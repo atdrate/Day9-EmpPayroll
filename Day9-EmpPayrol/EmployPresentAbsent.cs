c2d6f111d1cd5a56e14f0d3d44ddb5b27734d220
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-12 17:58:54 +0530

    [Subodh]Program for absent present check

---
+++ Day9-EmpPayrol/EmployPresentAbsent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployePayroll_Day10
+{
+    internal class EmployPresentAbsent
+    {
+        public static void EmployeeAttendance()
+        {
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == 0)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
---
+++ Day9-EmpPayrol/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace EmployePayroll_Day10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("chooes number\n1.EMp present absent\n2.Daily wages\n3.Part time wages\n4.using switch loop\n5. wages FOr Month\n6.using while loop\n7.Monthly emp wages\n8.wages for compnay");
+            int opt = Convert.ToInt32(Console.ReadLine());
+
+            switch (opt)
+            {
+                case 1:
+                    EmployPresentAbsent.EmployeeAttendance();
+                    break;
+                case 2:
+                    DailyWage.CalculateDailyWage();
+                    break;
+                case 3:
+                    ParttimeFullTime.CalculatePercentage();
+                    break;
+                case 4:
+                    SwitchLoop.CalculateWage();
+                    break;
+                case 5:
+                    WageForMonth.CalculateWageForMonth();
+                    break;
+                case 6:
+                    WhileLoop.CalculateWage();
+                    break;
+                case 7:
+                    EmpWagemonthly.CalculateEmpWages();
+                    break;
+                case 8:
+                    UsingMultipleCompany.ComputeWaageForMultipleCompany("Suzuki", 20, 20, 100);
+                    UsingMultipleCompany.ComputeWaageForMultipleCompany("BMW", 20, 20, 100);
+                    UsingMultipleCompany.ComputeWaageForMultipleCompany("Infy", 20, 20, 100);
+                    Console.WriteLine();
+                    break;
+               
+                    Console.WriteLine("Entered Wrong choice");
+                    break;
+
+            }
+        }
+    }
+}
